b3aade92ad01e3564acde5d3b855812c31d895b3
zwarag <harrys.kavan@pm.me>
2023-11-07 12:26:42 +0100

    async navigate

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -20,15 +20,14 @@ export class StepperComponent implements OnInit {
   route = inject(ActivatedRoute)
   router = inject(Router)
 
-  onStepChange(event: StepperSelectionEvent) {
+  async onStepChange(event: StepperSelectionEvent) {
     const stepRoute = ['step1', 'step2', 'step3'][event.selectedIndex];
-    this.router.navigate(['steps', stepRoute]);
+    await this.router.navigate(['steps', stepRoute]);
   }
 
   ngOnInit() {
     const stepMap = {'step1': 0, 'step2': 1, 'step3': 2};
     const currentStep = this.route.firstChild?.snapshot.url[0].path as keyof typeof stepMap;
-    0
     this.stepper.selectedIndex = stepMap[currentStep] ?? 0;
   }
 }
